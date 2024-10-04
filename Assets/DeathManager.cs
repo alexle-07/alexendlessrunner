346bf95038d88a5539f091685e9df23a0c0bfa13
alexle-07 <alehoang2024@elam.co.uk>
2024-10-04 15:33:35 +0100

    improved my game

---
+++ Assets/DeathManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathManager : MonoBehaviour
+{
+    public GameObject deathScreenCanvas;
+
+
+    public void ShowDeathScreen()
+    {
+        deathScreenCanvas.SetActive(true);
+
+        Time.timeScale = 1.0f;
+    }
+
+    // Update is called once per frame
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1f;
+        Application.Quit();
+    }
+}
---
+++ Assets/PlayerDeath.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float fallThreshold = -10f;
+    private DeathManager deathManager;
+
+
+    void Start()
+    {
+        deathManager = FindObjectOfType<DeathManager>();
+    }
+
+    void Update()
+    {
+        if (transform.position.y < fallThreshold)
+        {
+            deathManager.ShowDeathScreen();
+        }
+    }
+}
